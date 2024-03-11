9928cf014b2d928573b637a27a06948ef50c91af
Rebecca MUTONI <82598316+Beccatoni@users.noreply.github.com>
2024-03-11 18:01:52 +0100

    first 5 pages and slider

---
+++ Components/SlideItem.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SlideItem = ({item}) => {
+  return (
+    <View>
+      <View style={{backgroundColor:"#3F40AA", height:height, width:width, justifyContent:"space-around", alignItems:"center", padding:20}}>
+        <View style={{flexDirection:"row", gap:245, marginBottom:70}}>
+            <Image source={item.img} resizeMode="contain" style={{height:40, width:95}}/>
+            <Text style={{color:"white", fontWeight:450, }}>SKIP</Text>
+        </View>
+    <View style={{backgroundColor:"#2D2E9B", height:240, width:240, borderRadius:"100%", alignItems:"center", justifyContent:"center" }}>
+        <Image source={require('../assets/mag.png')} style={{height:170, width:215, borderRadius:22}}/>
+    </View>
+    <View style={{alignItems:"center", justifyContent:"center", gap:30}}><Text style={{textAlign:"center", color:"white", fontWeight:"bold"}}>Easy Top up & Withdraw</Text>
+      <Text style={{textAlign:"center", color:"white"}}>Enjoy your money after withdrawing. You can buy yourself a chicken wing or a good snack</Text></View>
+      
+    </View>
+    </View>
+  )
+}
+
+export default SlideItem
