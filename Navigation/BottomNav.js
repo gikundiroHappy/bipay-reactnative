77943102e0eb0eb45238c2cdf8f4ca6b3d72474c
Rebecca MUTONI <82598316+Beccatoni@users.noreply.github.com>
2024-03-14 09:10:04 +0100

    bottom navigation

---
+++ Navigation/BottomNav.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import History from '../Screens/History'
+import Deal from '../Screens/deals'
+
+const Tab = createBottomTabNavigator()
+
+const BottomNav = () => {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen  name='History' component={History}/>
+        <Tab.Screen name='Deal' component={Deal}/>
+    </Tab.Navigator>
+  )
+}
+
+export default BottomNav
